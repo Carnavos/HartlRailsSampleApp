c3e665343973351eec54c3548ba6a7945f944eb5
Theo Carnavos <tcarnavos@gmail.com>
2016-08-02 12:57:05 -0500

    Add title to all views, add setup method in StaticPagesControllerTest with base title string

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
+  # ells Rails to route a GET request for the URL /static_pages/about to the about action in the Static Pages controller
+  get 'static_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
