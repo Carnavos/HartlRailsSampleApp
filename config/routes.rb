bc196eceb8961fe346e93c801575a277b67e18f2
Theo Carnavos <tcarnavos@gmail.com>
2016-08-02 13:46:56 -0500

    Remove hello method from application controller, add contact test/route/controller method on static_pages/view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   # ells Rails to route a GET request for the URL /static_pages/about to the about action in the Static Pages controller
   get 'static_pages/about'
+  get 'static_pages/contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
 end
